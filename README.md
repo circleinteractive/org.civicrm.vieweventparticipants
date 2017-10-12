a3d813a3aaa763ac69f188c12471dd45327f7a27
davejenx <davej+git@circle-interactive.co.uk>
2017-10-12 15:16:09 +0100

    Implementing initial working version. (#1)

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # org.civicrm.vieweventparticipants
+
+## Overview
+
 CiviCRM extension granting access for event creators to view their events' participants.
+
+## Why would you need this extension?
+
+This extension is only needed if you have users who create events but who do not have permission to view all the participant contacts of those events. I.e. the event creators do not have "view all contacts" permission and do not have permission via ACLs (access control lists) to view all the relevant contacts.
+
+## What does this extension do?
+
+It implements a new permission, 'view my event participants'. This grants the user access to all the contacts who are participants of events that were created by the user.
+
+**N.B.** If other ACLs are in place, e.g. through CiviCRM's "Manage ACLs" user interface, then this extension allows access to the user's events' participants _in addition to_ the contacts permitted by these other ACLs. Consider whether this is what you want: in some use cases, you might instead want to grant access to view only the participants who are also permitted by the other ACLs.
+
+E.g. suppose there's an existing ACL that gives a user access to all contacts with a UK address. With this extension, that user would have access to all UK contacts _plus_ all participants of events created by the user - even if those participants are outside the UK.
+
+**Note:** access is granted by means of a hook. Consequently this will not show up in the CiviCRM user interface under Manage ACLs: it's done "under the hood".
+
+## How do I use it?
+
+1. Install the extension.
+Currently this extension is not available for automated distribution through CiviCRM's Extension management screen and so must be manually downloaded onto the server, from https://github.com/circleinteractive/org.civicrm.vieweventparticipants .
+2. Enable the extension.
+From the CiviCRM menu, go to Administer -> System settings -> Extensions. Click the **Enable** link for View Event Participants (org.civicrm.vieweventparticipants).
+3. Grant permissions.
+In your CMS's permissions management screen, grant the 'view my event participants' permission to the users/roles who need to be able to view their event participants.
---
+++ vieweventparticipants.php
@@ -0,0 +1,166 @@
+<?php
+
+require_once 'vieweventparticipants.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function vieweventparticipants_civicrm_config(&$config) {
+  _vieweventparticipants_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function vieweventparticipants_civicrm_xmlMenu(&$files) {
+  _vieweventparticipants_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function vieweventparticipants_civicrm_install() {
+  _vieweventparticipants_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function vieweventparticipants_civicrm_uninstall() {
+  _vieweventparticipants_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function vieweventparticipants_civicrm_enable() {
+  _vieweventparticipants_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function vieweventparticipants_civicrm_disable() {
+  _vieweventparticipants_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function vieweventparticipants_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _vieweventparticipants_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function vieweventparticipants_civicrm_managed(&$entities) {
+  _vieweventparticipants_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function vieweventparticipants_civicrm_caseTypes(&$caseTypes) {
+  _vieweventparticipants_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function vieweventparticipants_civicrm_angularModules(&$angularModules) {
+  _vieweventparticipants_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function vieweventparticipants_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _vieweventparticipants_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_permission().
+ *
+ * @link https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_permission/
+ */
+function vieweventparticipants_civicrm_permission(&$permissions) {
+  $permissions['view my event participants'] = array(
+    ts('CiviEvent: view my event participants', array('domain' => 'org.civicrm.vieweventparticipants')),
+    ts('Grants event creators permission to view their event\'s participants', array('domain' => 'org.civicrm.vieweventparticipants')),
+  );
+}
+
+/**
+ * Implements hook_civicrm_aclWhereClause().
+ *
+ * @link https://docs.civicrm.org/dev/en/master/hooks/hook_civicrm_aclWhereClause/
+ */
+function vieweventparticipants_civicrm_aclWhereClause($type, &$tables, &$whereTables, &$contactID, &$where) {
+  /*
+   * Grant access for event creators to view their events' participants.
+   */
+  if (!$contactID) {
+    return;
+  }
+
+  // Only allow access if user also has our permission.
+  if (!CRM_Core_Permission::check('view my event participants')) {
+    return;
+  }
+
+  if (!in_array('civicrm_participant', $whereTables)) {
+    $tables['civicrm_participant'] = $whereTables['civicrm_participant']
+      = "LEFT JOIN civicrm_participant ON contact_a.id = civicrm_participant.contact_id";
+  }
+
+  if (!in_array('civicrm_event', $whereTables)) {
+    $tables['civicrm_event'] = $whereTables['civicrm_event']
+      = "LEFT JOIN civicrm_event ON civicrm_participant.event_id = civicrm_event.id";
+  }
+
+  /*
+   * If other ACLs are in place, e.g. through ACL UI, then we allow access to
+   * the user's events' participants in addition to the contacts permitted by
+   * these other ACLs. Hence OR.
+   */
+  if (!empty($where)) {
+    $where = "($where) OR ";
+  }
+
+  $where .= sprintf("(civicrm_event.created_id = %d)", $contactID);
+}
